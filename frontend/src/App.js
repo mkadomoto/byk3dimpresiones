dafa649dc43a46ce9f0d86f72078f719c1753520
mkadomoto <mkadomoto@frba.utn.edu.ar>
2026-03-16 23:05:01 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,46 @@
 import "@/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import StorePage from "./pages/StorePage";
 import LoginPage from "./pages/LoginPage";
 import AdminPage from "./pages/AdminPage";
+
+function PrivateRoute({ children }) {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/tienda" element={<StorePage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route
+        path="/admin"
+        element={
+          <PrivateRoute>
+            <AdminPage />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <AuthProvider>
       <div className="App">
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/tienda" element={<StorePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-          </Routes>
+          <AppRoutes />
         </BrowserRouter>
       </div>
     </AuthProvider>
